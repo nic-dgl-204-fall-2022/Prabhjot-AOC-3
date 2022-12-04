36f8de340b1b585d63c50a318ed55d992840f2ed
Prabhjot <61209227+08prabhjot@users.noreply.github.com>
2022-12-04 12:43:03 +0530

    Added Second Solution Code

--- AOC3/src/main/kotlin/Day16.kt
+++ AOC3/src/main/kotlin/Day16.kt
@@ -6,34 +6,46 @@ fun main() {
     val stringInput = readLine() ?: ""
 
     val orders = stringInput.split(",")
-    val i = 1
+    var i = 1
 
+    val iterations = 1_000_000_000
 
-    orders.forEach { order ->
-        when (order[0]) {
-            's' -> spinDanceMove(Integer.parseInt(order.slice(1 until order.length)))
-            'x' -> {
-                val orderExchangeKey = order.split("/")
-                val firstPosition =
-                    Integer.parseInt(orderExchangeKey[0].slice(1 until orderExchangeKey[0].length))
-                val secondPosition = Integer.parseInt(orderExchangeKey[1])
-                exchangeDanceMove(firstPosition, secondPosition)
+    val copyingProgChar = programsChar.toList()
+
+    while (i <= iterations) {
+        orders.forEach { order ->
+            when (order[0]) {
+                's' -> spinDanceMove(Integer.parseInt(order.slice(1 until order.length)))
+                'x' -> {
+                    val orderExchangeKey = order.split("/")
+                    val firstPosition =
+                        Integer.parseInt(orderExchangeKey[0].slice(1 until orderExchangeKey[0].length))
+                    val secondPosition = Integer.parseInt(orderExchangeKey[1])
+                    exchangeDanceMove(firstPosition, secondPosition)
+                }
+                'p' -> partnerDanceMove(order[1], order[3])
+                else -> println("This $order is not known.")
             }
-            'p' -> partnerDanceMove(order[1], order[3])
-            else -> println("This $order is not known.")
         }
+        if (i == 1) {
+            val stringFromPrograms = programsChar.joinToString("")
+            println("First Part's result- $stringFromPrograms")
+        }
+        if (programsChar == copyingProgChar) {
+            val iterationFactor = iterations / i
+            i = i * iterationFactor
+        }
+        i++
     }
-    if (i == 1) {
-        val stringFromPrograms = programsChar.joinToString("")
-        println("So, answer is: $stringFromPrograms")
-    }
-
+    val programsString = programsChar.joinToString("")
+    println("Second Part's result- $programsString")
 }
 
-fun spinDanceMove(postionNumber: Int) {
-    programsChar = ((programsChar.slice((programsChar.size - postionNumber) until programsChar.size))
+fun spinDanceMove(positionNumber: Int) {
+
+    programsChar = ((programsChar.slice((programsChar.size - positionNumber) until programsChar.size))
             +
-            (programsChar.slice(0 until (programsChar.size - postionNumber)))).toMutableList()
+            (programsChar.slice(0 until (programsChar.size - positionNumber)))).toMutableList()
 }
 
 fun exchangeDanceMove(firstPosition: Int, secondPosition: Int) {
